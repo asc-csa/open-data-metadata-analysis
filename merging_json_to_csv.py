1ab1fd8a018f2dfb16a8b2cda147612be8964a86
Haiqa Arain <106987975+haiqa-arain@users.noreply.github.com>
2022-09-16 13:39:49 -0400

    Update merging_json_to_csv.py

--- merging_json_to_csv.py
+++ merging_json_to_csv.py
@@ -1,10 +1,4 @@
-import json
-import numpy as np
-import csv
 import pandas as pd
-from csv import writer
-from csv import reader
-
 
 df1 = pd.read_json('ckan_metadata_dump_20220526.json', lines=True)
 df1.head()
@@ -12,6 +6,7 @@ df1.head()
 df2 = pd.read_csv('ckan_views_20220526.csv')
 df2.head()
 
+#appending json to dataframe
 df3 = pd.merge(df1, df2, how='left', left_on='name', right_on='dataset name')
 df3.head()
 
